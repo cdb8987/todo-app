5e22e14aa8e378f5e26fcb47e0859f7856333dae
Charlie <cdb8987@gmail.com>
2023-01-15 10:50:17 -0500

    switched to appending html P elements rather than a list
    input form now appends/injects 1 new p element with the input value to the HTML

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-let toDoList = [];  //create empty array
-
 const submitTodo = document.getElementById('todoinputsubmit'); // create a pointer to the html element #todoinput for later click event
 
+let listDisplay = document.getElementById("todolist");
 
 const addFunction = function () {
-    const inputValue = document.querySelector("#todoinput").value
-    toDoList.push(inputValue)
-    document.querySelector("#todoinput").value = '' //resets form to empty string
-    displayList()
-}
-const displayList = function () {
-    for (let index = 0; index < toDoList.length; index++) {
-        let element = toDoList[index];
-        element = '<li>' + element + '</li>';
-        toDoList[index] = element
-
-    }
-    document.getElementById('todolist').innerHTML = toDoList
+    let inputValue = document.querySelector("#todoinput").value;
+    const inputHTML = `<p id="${inputValue}">` + inputValue + '</p>'  //this is the key element...we need to assign id, class and css options along with event listener
+    listDisplay.innerHTML += inputHTML
+    inputValue = ''
+    console.log('addFunction Ran')
+    console.log(inputHTML)
 }
 
 submitTodo.addEventListener('click', addFunction);
