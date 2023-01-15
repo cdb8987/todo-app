2f77bdc293ccd14575fa2c9e464e42fd94488976
Charlie <cdb8987@gmail.com>
2023-01-15 15:53:07 -0500

    fixed parameter passing issue and made onlick function change p tage class to strikethrough

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const addTodo = document.getElementById('todoinputsubmit'); // create a pointer to the html element #todoinput for later click event
+const addTodo = document.getElementById('todoinputsubmit'); // create a pointer to the html form element #todoinput for later click event
 
 let listDisplay = document.getElementById("todolist");
 
@@ -7,23 +7,24 @@ const addFunction = function () {
     const inputHTML = `<p id="${inputValue}">` + inputValue + '</p>'  //this is the key element...we need to assign id, class and css options along with event listener
     listDisplay.innerHTML += inputHTML
 
-    newHTML = document.getElementById(`${inputValue}`)
+    const newHTML = document.getElementById(`${inputValue}`)
     newHTML.addEventListener('click', () => {
         changeToCompleted(inputValue)
     })
 
-    inputValue = ''
-    console.log('addFunction Ran')
-    console.log(inputHTML)
+
 }
 
 addTodo.addEventListener('click', addFunction);
 
 const changeToCompleted = function (inputValue) {
-    console.log('changeToCompleted started')
-    pTag = document.getElementById(inputValue)
+    console.log('changeToCompleted started', 'input value is', inputValue)
+    pTag = document.getElementById(inputValue).innerText
+    document.getElementById(inputValue).classList.add('complete')
     console.log('p tag is', pTag)
-    console.log('changeToCompleted finished')
+    document.getElementById('todoinput').innerText = ''
+    console.log('changeToCompleted finished', 'input value is', inputValue)
+
 
 }
 
