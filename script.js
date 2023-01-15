78de14371f9d4a5b454c1507a4169038af67e779
Charlie <cdb8987@gmail.com>
2023-01-15 11:15:37 -0500

    created an event listener for the created ptag
    having issues passing inputValue as a parameter to changetoCompleted function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const submitTodo = document.getElementById('todoinputsubmit'); // create a pointer to the html element #todoinput for later click event
+const addTodo = document.getElementById('todoinputsubmit'); // create a pointer to the html element #todoinput for later click event
 
 let listDisplay = document.getElementById("todolist");
 
@@ -6,12 +6,27 @@ const addFunction = function () {
     let inputValue = document.querySelector("#todoinput").value;
     const inputHTML = `<p id="${inputValue}">` + inputValue + '</p>'  //this is the key element...we need to assign id, class and css options along with event listener
     listDisplay.innerHTML += inputHTML
+
+    newHTML = document.getElementById(`${inputValue}`)
+    newHTML.addEventListener('click', () => {
+        changeToCompleted(inputValue)
+    })
+
     inputValue = ''
     console.log('addFunction Ran')
     console.log(inputHTML)
 }
 
-submitTodo.addEventListener('click', addFunction);
+addTodo.addEventListener('click', addFunction);
+
+const changeToCompleted = function (inputValue) {
+    console.log('changeToCompleted started')
+    pTag = document.getElementById(inputValue)
+    console.log('p tag is', pTag)
+    console.log('changeToCompleted finished')
+
+}
+
 
 
 
