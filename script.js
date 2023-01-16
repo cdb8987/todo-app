56d8d57ec33b2743277088a626ef486e1710be4b
Charlie <cdb8987@gmail.com>
2023-01-16 18:04:16 -0500

    code meets specifications
    added event listener for p tags that would change style to strikethrough and add a child button.  Added an event listener for the child button that would change display on the p tag to none.  Added boolean checking if the p tags class was crossed off in order to prevent multiple exit buttons from  being generated from multiple clicks.

--- script.js
+++ script.js
@@ -2,14 +2,26 @@ const addTodo = document.getElementById('todoinputsubmit'); // create a pointer 
 
 let listDisplay = document.getElementById("todolist");
 
-const addFunction = function () {
+const addFunction = function () {  //this fires every time 'add' is clicked
     let inputValue = document.querySelector("#todoinput").value;
     const inputHTML = `<p id="${inputValue}">` + inputValue + '</p>'  //this is the key element...we need to assign id, class and css options along with event listener
     listDisplay.innerHTML += inputHTML
+    const pTags = listDisplay.querySelectorAll('p');
+    pTags.forEach((p) => {
+        p.addEventListener('click', () => {
+            p.style['text-decoration'] = 'line-through'
+            if (p.className != 'crossedoff') {
+                p.className = 'crossedoff'
+                p.innerHTML += '<button>X</button>'
+                const pButton = p.getElementsByTagName('button')[0]
+                console.log('PBUTTON IS', pButton)
+                pButton.addEventListener('click', () => {
+                    console.log('button event triggered')
+                    p.style['display'] = 'none'
+                })
+            }
 
-    const newHTML = document.getElementById(`${inputValue}`)
-    newHTML.addEventListener('click', () => {
-        changeToCompleted(inputValue)
+        })
     })
 
 
@@ -17,16 +29,6 @@ const addFunction = function () {
 
 addTodo.addEventListener('click', addFunction);
 
-const changeToCompleted = function (inputValue) {
-    console.log('changeToCompleted started', 'input value is', inputValue)
-    pTag = document.getElementById(inputValue).innerText
-    document.getElementById(inputValue).classList.add('complete')
-    console.log('p tag is', pTag)
-    document.getElementById('todoinput').innerText = ''
-    console.log('changeToCompleted finished', 'input value is', inputValue)
-
-
-}
 
 
 
